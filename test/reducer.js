cf95392f9ad428451f8e2090f5336093bc148f9d
Brandon Silva <itbrandonsilva@gmail.com>
2016-12-11 12:23:55 -0500

    Reword tests.

--- test/reducer.js
+++ test/reducer.js
@@ -21,19 +21,19 @@ describe('reducer', () => {
             expect(state).to.be.an.instanceof(Immutable.Map);
         });
 
-        it('that has a "players" key pointing to an Immutable.Map', () => {
+        it('that has a "players" key pointing to an instance of Immutable.Map', () => {
             expect(state.get('players')).to.be.an.instanceof(Immutable.Map);
         });
 
-        it('that has a "spectators" key pointing to an object', () => {
+        it('that has a "spectators" key pointing to an instance of Immutable.Map', () => {
             expect(state.get('spectators')).to.be.an.instanceof(Immutable.Map);
         });
 
-        it('that has a "world" key pointing to an Immutable.Map', () =>  {
+        it('that has a "world" key pointing to an instance of Immutable.Map', () =>  {
             expect(state.get('world')).to.be.an.instanceof(Immutable.Map);
         });
 
-        it('that has an Immutable.List representing a 6x6 grid', () => {
+        it('where the "world" Immutable.Map has a key "map" pointing to an Immutable.List representing a 6x6 grid', () => {
             let map = state.get('world').get('map');
             expect(map).to.be.an.instanceof(Immutable.List);
             map.size.should.equal(6 * 6);
