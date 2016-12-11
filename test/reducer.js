624d445b471869873150ddfec019b152c688ee4a
Brandon Silva <itbrandonsilva@gmail.com>
2016-12-11 12:03:15 -0500

    Add tests.

--- test/reducer.js
+++ test/reducer.js
@@ -15,12 +15,26 @@ describe('reducer', () => {
             state = store.getState();
         });
 
-        it('that has a "players" key pointing to an object', () => {
+        it('that is an Immutable.Map', () => {
+            expect(state).to.be.an.instanceof(Immutable.Map);
+        });
+
+        it('that has a "players" key pointing to an Immutable.Map', () => {
             expect(state.get('players')).to.be.an.instanceof(Immutable.Map);
         });
 
         it('that has a "spectators" key pointing to an object', () => {
             expect(state.get('spectators')).to.be.an.instanceof(Immutable.Map);
-        })
+        });
+
+        it('that has a "world" key pointing to an Immutable.Map', () =>  {
+            expect(state.get('world')).to.be.an.instanceof(Immutable.Map);
+        });
+
+        it('that has an Immutable.List representing a 6x6 grid', () => {
+            let map = state.get('world').get('map');
+            expect(map).to.be.an.instanceof(Immutable.List);
+            map.size.should.equal(6 * 6);
+        });
     });
 });
